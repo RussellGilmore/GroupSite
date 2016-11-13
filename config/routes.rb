433784117cefbf5adcddd6234fa55c614cf152db
Russell Gilmore <RussellGilmore@live.com>
2016-11-13 18:10:14 -0500

    Fixed bug where footer would overlap the content section. Created new application level document layout.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get 'pages/text_chat'  => 'pages#text_chat'
     get 'pages/drive'      => 'pages#drive'
     get 'pages/video_chat' => 'pages#video_chat'
-    resources :posts
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :posts do
+      resources :comments
+    end
 end
