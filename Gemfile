67a23d8cd064d9ae1e48ce5fba27948adf44ff52
Russell Gilmore <RussellGilmore@live.com>
2016-11-12 20:49:29 -0500

    Added CRUD for forum. Added Gem Simple Forum. Added Controller Posts for Forum views.

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'bootstrap-sass', '~> 3.3.6'
 
 gem 'figaro'
 
+gem 'simple_form', '~> 3.3', '>= 3.3.1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923211046) do
+ActiveRecord::Schema.define(version: 20161113011202) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
