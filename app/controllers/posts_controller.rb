f2e76e23a2c167b3590309024dff709ef7adab30
Russell Gilmore <RussellGilmore@live.com>
2016-11-13 23:01:23 -0500

    Created working forum with posts comments and associated CRUD. Still need to add timestamps and user information for  posts. Added rubocop gem that is used for stylizing code with beautify.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,50 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
 
-def forum_index
-  @posts = Post.all.order("created_at DESC")
-end
+    def forum_index
+        @posts = Post.all.order('created_at DESC')
+    end
 
-def show
-  @post = Post.find(params[:id])
-end
+    def show
+        @post = Post.find(params[:id])
+    end
 
-def new
-  @post = Post.new
-end
+    def new
+        @post = Post.new
+    end
 
-def create
-  @post = Post.new(post_params)
-  if @post.save
-      redirect_to root_path
-  else
-    render 'new'
-  end
-end
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
 
-def edit
+    def edit
+      end
 
-end
+    def update
+        if @post.update(post_params)
+            redirect_to post_path
+        else
+            render 'edit'
+        end
+    end
 
-def update
-  if @post.update(post_params)
-    redirect_to post_path
-  else
-    render 'edit'
-  end
-end
+    def destroy
+        @post.destroy
+        redirect_to root_path
+    end
 
-def destroy
-  @post.destroy
-  redirect_to root_path
-end
+    private
 
-private
+    def post_params
+        params.require(:post).permit(:title, :description)
+    end
 
-def post_params
-  params.require(:post).permit(:title, :description)
-end
-
-def find_post
-  @post = Post.find(params[:id])
-end
+    def find_post
+        @post = Post.find(params[:id])
+    end
 end
