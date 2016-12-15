adb77ebf923db683570d7ad479d7c03bd704a150
Kerwin Yoder <kerwinyoder@gmail.com>
2016-12-15 12:31:34 -0500

    Fixes the bug where Dropzone shows fallbacks because of Turbolinks

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -1,9 +1,8 @@
-$(function () {
-  Dropzone.autoDiscover = false;
-
-  $("#my-dropzone").dropzone({
-    maxFilesize: 2,
-    addRemoveLinks: true,
-    paramName: 'upload[image]'
-  })
+$(function() {
+    $(document).on('turbolinks:load', Dropzone.discover);
+    $("#my-dropzone").dropzone({
+        maxFilesize: 2,
+        addRemoveLinks: true,
+        paramName: 'upload[image]'
+    })
 })
