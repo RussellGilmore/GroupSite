RussellGilmore/GroupSite

commit 765daec35493b5da5da7895922f76909b3acc966

Adds the registration key requirement to the signup process
Registration keys can be created from the rails console using the
following commands.
rails c
RegistrationKey.create

app/models/user.rb
@@ -6,19 +6,31 @@ class User < ApplicationRecord
            :confirmable, :lockable, :timeoutable,
            :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
+    validates :username,
+              presence: true,
+              uniqueness: {
+                  case_sensitive: false
+              }
+    validates :registration_key,
+              on: :create,
+              presence: true
+    validate :registration_key_exists
+
+    # Only allow letter, number, underscore and punctuation.
+    # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+
+    after_create :delete_registration_key
+
     # Add a virtual attribute for authenticating by either username or email
     # This field is not persisted. It is only used in controllers and views.
     attr_accessor :login
+    attr_accessor :registration_key
 
     # Maps the data from the OAuth response to the User model
     def self.from_omniauth(auth)
-        username = auth.info.email.split(/@/)[0]
-        already_used = User.where(username: username).first
-        username = auth.info.email if already_used
-        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.username = username
+        where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
+            user.username = auth.info.nickname if auth.info.nickname
             user.email = auth.info.email
-            user.password = Devise.friendly_token[0, 20]
         end
     end
 
@@ -36,11 +48,15 @@ class User < ApplicationRecord
         end
     end
 
-    validates :username,
-              presence: true,
-              uniqueness: {
-                  case_sensitive: false
-              }
-    # Only allow letter, number, underscore and punctuation.
-    # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+    # validates that the registration key exists.
+    def registration_key_exists
+        key = RegistrationKey.find_by_key registration_key
+        errors.add(:registration_key, 'is not a valid registration key') unless key
+    end
+
+    # Removes the registration key after_create to prevent reuse
+    def delete_registration_key
+        key = RegistrationKey.find_by_key registration_key
+        key.destroy if key
+    end
 end

app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
     # Configure the parameters that are permitted for sign up and account updates.
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit :sign_up, keys: [:username, :email, :password, :password_confirmation]
+        devise_parameter_sanitizer.permit :sign_up, keys: [:registration_key, :username, :email, :password, :password_confirmation]
         devise_parameter_sanitizer.permit :account_update, keys: [:username, :email, :current_password], except: [:password, :password_confirmation]
     end
 end

app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,61 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    # Handles callbacks for Facebook sign in
+    # Users are redirected to the registration page
     def facebook
-        @user = User.from_omniauth(request.env['omniauth.auth'])
+        auth = request.env['omniauth.auth']
+        @user = User.from_omniauth(auth)
 
         if @user.persisted?
             sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
             set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
         else
-            session['devise.facebook_data'] = request.env['omniauth.auth']
-            redirect_to new_user_registration_url
+            # Store the provider and uid in the session for use in registration
+            session[:provider] = auth.provider
+            session[:uid] = auth.uid
+            render 'users/new'
         end
     end
 
+    # Handles callbacks for Google` sign in
+    # Users are redirected to the registration page
     def google_oauth2
-        @user = User.from_omniauth(request.env['omniauth.auth'])
+        auth = request.env['omniauth.auth']
+        @user = User.from_omniauth(auth)
 
         if @user.persisted?
             sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
             set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
         else
-            session['devise.google_data'] = request.env['omniauth.auth']
-            redirect_to new_user_registration_url
+            # Store the provider and uid in the session for use in registration
+            session[:provider] = auth.provider
+            session[:uid] = auth.uid
+            render 'users/new'
         end
+    end
+
+    # Creates a new user for Facebook and Google registration.
+    def create
+        @user = User.new(user_params)
+        @user.password = Devise.friendly_token[0, 20]
+        @user.provider = session[:provider]
+        @user.uid = session[:uid]
+        @user.save
+        if @user.persisted?
+            session.delete :provider
+            session.delete :uid
+            sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+            set_flash_message(:notice, :success, kind: @user.provider == :facebook ? 'Facebook' : 'Google') if is_navigational_format?
+        else
+            render 'users/new'
       end
+    end
 
     def failure
         redirect_to controller: 'devise/sessions', action: 'new'
+    end
+
+    # The permitted parameters for user registration
+    def user_params
+        params.require(:user).permit(:registration_key, :username, :email)
     end
 end

app/views/devise/registrations/new.html.erb
@@ -4,8 +4,12 @@
   <%= devise_error_messages! %>
 
   <div class="field">
+    <%= f.label :registration_key %><br/>
+    <%= f.text_field :registration_key, autofocus: true %>
+  </div>
+  <div class="field">
     <%= f.label :username %><br/>
-    <%= f.text_field :username, autofocus: true %>
+    <%= f.text_field :username %>
   </div>
 
   <div class="field">

app/views/users/new.html.erb
@@ -1,0 +1,25 @@
+<h2>Sign up</h2>
+
+<%= form_for(@user, url: users_create_url ) do |f| %>
+  <%= devise_error_messages! %>
+
+  <div class="field">
+    <%= f.label :registration_key %><br/>
+    <%= f.text_field :registration_key, autofocus: true %>
+  </div>
+  <div class="field">
+    <%= f.label :username %><br/>
+    <%= f.text_field :username %>
+  </div>
+
+  <div class="field">
+    <%= f.label :email %><br/>
+    <%= f.email_field :email%>
+  </div>
+
+  <div class="actions">
+    <%= f.submit "Sign up" %>
+  </div>
+<% end %>
+
+<%= render "devise/shared/links" %>

config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
     devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+    devise_scope :user do
+        post 'users/create', to: 'users/omniauth_callbacks#create', as: 'users_create'
+    end
     # redirect to the sign in page if the user is not authenticated
     authenticated :user do
         root to: 'posts#forum_index', as: :authenticated_root

commit aa968a256eb175eb1649c7af122e7f7f18df5e12

Fixes a bug that prevented users from modifying their account without a registration key.
This bug prevented users from modifying their account, changing passwords,
etc. without a registration key. The bug was introduced in 765daec.

app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :registration_key,
               on: :create,
               presence: true
-    validate :registration_key_exists
+    validate :registration_key_exists, on: :create
 
     # Only allow letter, number, underscore and punctuation.
     # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true